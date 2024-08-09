66dd1155a04d246e583beedc5a20cdf8bf0b4f40
Jan Koster <jan.koester@tuxist.de>
2024-08-09 12:54:03 +0200

    fixed

--- src/libnetplusConfig.cmake
+++ src/libnetplusConfig.cmake
@@ -2,3 +2,15 @@ include(CMakeFindDependencyMacro)
 # find_dependency(xx 2.0)
 include(${CMAKE_CURRENT_LIST_DIR}/libnetplusTargets.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/libnetplusVersion.cmake)
+
+set_property(
+    TARGET netplus
+    APPEND PROPERTY
+        INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}"
+)
+
+set_property(
+    TARGET netplus-static
+    APPEND PROPERTY
+        INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}"
+)
